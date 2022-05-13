1e32192a3f47c79ace0dc6005c2fa2a90d1241e8
Jose Luis Bravo <bravo.jose.luis18@gmail.com>
2022-05-13 18:42:06 -0300

    add styles

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,23 @@
-new gridjs.Grid({
-  columns: ['Name', 'Email', 'Phone Number'],
-  data: [
-    ['John', 'john@example.com', '(353) 01 222 3333'],
-    ['Mark', 'mark@gmail.com',   '(01) 22 888 4444']
-  ]
-}).render(document.getElementById("wrapper"));
+const columnDefs = [
+  {headerName: '#', field: 'id'},
+  {headerName: 'Nombre', field: 'name'},
+  {headerName: 'Precio', field: 'price'},
+  {headerName: '1D Cambio', field: '1dChange'},
+  {headerName: '7D Prediccion', field: '7dPrediction'},
+  {headerName: 'Tapa del Mercado', field: 'marketCap'},
+  {headerName: 'Volumen', field: 'volume'},
+  {headerName: '1D grafico', field: '1dChart'},
+  {headerName: 'CSV Datos', field: 'csvData'},
+];
+const rowData = [
+  {make: 'Toyota', model: 'Celica', price: 35000},
+  {make: 'Ford', model: 'Mondeo', price: 32000},
+  {make: 'Porsche', model: 'Boxter', price: 72000}
+];
+const gridOptions = {
+  columnDefs: columnDefs,
+  rowData: rowData
+};
+
+const eGridDiv = document.querySelector('#myGrid');
+new agGrid.Grid(eGridDiv, gridOptions);
